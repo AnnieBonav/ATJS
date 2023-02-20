59c3de399a28e1e5c2d307f84f706cb8e9eedee5
AnnieBonav <anniebonav@outlook.com>
2023-02-20 17:18:30 +0100

    Exercise 8 done

--- class/vue-app/src/pages/Guesser.vue
+++ class/vue-app/src/pages/Guesser.vue
@@ -1,10 +1,12 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, computed, onBeforeMount } from "vue";
+import Loader from "@/components/Loader.vue";
 
-console.log('here 2');
+console.log("here 2");
 
-const deckID = "681glf5wrfni";
-const lastCard = ref(null);
+const deckID = ref();
+const lastCard = ref();
+const loading = ref(true);
 
 const score = ref(0);
 const currentGuess = ref();
@@ -14,10 +16,30 @@ const currentGuess = ref();
 // Draw a card
 
 const drawCardAPI = computed(() => {
-  return `https://www.deckofcardsapi.com/api/deck/${deckID}/draw/?count=1`;
+  return `https://www.deckofcardsapi.com/api/deck/${deckID.value}/draw/?count=1`;
 });
 
+const newCardAPI =
+  "https://www.deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+
+onBeforeMount(() => {
+  setTimeout(async () => {
+    const { deck_id } = await fetch(newCardAPI).then((r) => r.json());
+    deckID.value = deck_id;
+    loading.value = false;
+  }, 2000);
+});
+
+async function getNewDeckId() {
+  const { deck_id } = await fetch(newCardAPI).then((r) => r.json());
+  deckID.value = deck_id;
+  console.log("set deckId to", deck_id);
+}
+
 async function drawCard() {
+  if (!deckID.value) {
+    return;
+  }
   const { cards } = await fetch(drawCardAPI.value).then((r) => r.json());
   lastCard.value = cards[0];
 
@@ -25,12 +47,12 @@ async function drawCard() {
 }
 
 function validateResult(suit) {
-  const reds = ['HEARTS', 'DIAMONDS'];
-  const blacks = ['SPADES', 'CLUBS'];
+  const reds = ["HEARTS", "DIAMONDS"];
+  const blacks = ["SPADES", "CLUBS"];
 
-  console.log('Validate', suit, currentGuess.value);
+  console.log("Validate", suit, currentGuess.value);
 
-  if (currentGuess.value == 'RED') {
+  if (currentGuess.value == "RED") {
     return reds.includes(suit) ? score.value++ : null;
   }
   return blacks.includes(suit) ? score.value++ : null;
@@ -42,25 +64,32 @@ function validateResult(suit) {
 <template>
   <h3>Current Score {{ score }}</h3>
   <h3>Current Guess {{ currentGuess }}</h3>
+  <h3>Current Deck {{ deckID }}</h3>
 
-  <button
-    @click="
-      () => {
-        currentGuess = 'RED';
-      }
-    ">
-    Guess Red
-  </button>
-  <button
-    @click="
-      () => {
-        currentGuess = 'BLACK';
-      }
-    ">
-    Guess Black
-  </button>
+  <Loader v-if="loading" />
+  <div v-else>
+    <button
+      @click="
+        () => {
+          currentGuess = 'RED';
+        }
+      "
+    >
+      Guess Red
+    </button>
+    <button
+      @click="
+        () => {
+          currentGuess = 'BLACK';
+        }
+      "
+    >
+      Guess Black
+    </button>
 
-  <button v-if="currentGuess" @click="drawCard">Draw Card</button>
+    <button v-if="currentGuess" @click="drawCard">Draw Card</button>
+    <button @click="getNewDeckId">Get New Deck</button>
 
-  <img v-if="lastCard" :src="lastCard.image" height="150" />
+    <img v-if="lastCard" :src="lastCard.image" height="150" />
+  </div>
 </template>
